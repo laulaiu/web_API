041c6c8a58ffb2704e7025e4f521afdcf651677a
Laulaiu <Laulaiu14@gmail.com>
2022-01-20 09:34:58 -0300

    Adicionar arquivos de projeto.

---
+++ web_API/Controllers/ClientesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using web_API.Models;
+
+namespace web_API.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        [HttpGet]
+        public List<Cliente> ChamadaGet()
+        {
+            return clientes;
+        }
+
+        [HttpPost]
+        public void ChamadaPost(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+            {
+                clientes.Add(new Cliente(nome));
+            }            
+        }
+        
+        [HttpDelete]
+        public void Chamadadelete(string nome)
+        {            
+            clientes.RemoveAt(clientes.IndexOf(clientes.Find(x => x.nome.Equals(nome))));
+        }
+
+    }
+}
---
+++ web_API/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_API.Models
+{
+    public class Cliente
+    {
+        public string nome { get; set; }
+
+        public Cliente(String nome)
+        {
+            this.nome = nome;
+
+        }
+
+    }
+}
